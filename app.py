dab0c80ff8935bd7fed28327645e0b5894c443cb
halcy8312 <157008050+halcy8312@users.noreply.github.com>
2024-06-12 03:17:50 +0900

    コントロール6

--- app.py
+++ app.py
@@ -691,12 +691,6 @@ def controled():
     image_filename = request.args.get('image_filename')
     return render_template('controled.html', image_filename=image_filename)
 
-@app.route('/download_image/<filename>')
-def download_image(filename):
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    return send_file(file_path, as_attachment=True)
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
